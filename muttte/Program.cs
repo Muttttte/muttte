2a4fdf512184bc5ea091626dae16b42321b57241
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-16 13:56:32 +0200

    Add project files.

---
+++ muttte/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace _2_11
+{
+
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hur långt hoppade Elin i meter?");
+            string x = Console.ReadLine();
+            Console.WriteLine("hur långt har Alma hoppat i meter?");
+            string z = Console.ReadLine();
+
+
+            int y = int.Parse(z);
+            Console.WriteLine("Elin hoppade " + x);
+        }
+
+    }
+
+}
+                 
+       
